4eaa2c400d324092c84bcd7ceb574370f9242495
seunghoon <tmdgns4321@gmail.com>
2022-10-09 20:10:01 +0900

    리덕스 적용 전

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+import { useRef, useState } from "react";
+import styled from "styled-components";
 
 function App() {
+  const [title, setTitle] = useState("");
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      title: "연습중..",
+    },
+  ]);
+  const newId = useRef(2);
+  const onChangeHandler = (e) => {
+    setTitle(e.target.value);
+  };
+
+  const onClickHandler = () => {
+    if (!title) return;
+    setTodos([...todos, { id: (newId.current += 1), title: title }]);
+    setTitle("");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <FirstContainer>
+        <Label htmlFor="todoTitle">Todos의 제목을 입력하세요</Label>
+        <InputTitle
+          type="text"
+          value={title}
+          id="todoTitle"
+          onChange={onChangeHandler}
+        ></InputTitle>
+        <InputBtn onClick={onClickHandler}>추가하기</InputBtn>
+      </FirstContainer>
+      <ContainerDiv>
+        {todos.map((todo) => (
+          <TitleDiv key={todo.id}>{todo.title}</TitleDiv>
+        ))}
+      </ContainerDiv>
     </div>
   );
 }
+
+const FirstContainer = styled.div`
+  margin: 20px;
+`;
+
+const Label = styled.label`
+  margin-right: 30px;
+`;
+
+const InputTitle = styled.input`
+  width: 200px;
+  height: 20px;
+  border: 1px solid lightgray;
+  border-radius: 50px;
+  margin-right: 30px;
+`;
+
+const InputBtn = styled.button`
+  width: 100px;
+  height: 25px;
+  background-color: lightgray;
+  border: none;
+  border-radius: 50px;
+`;
+
+const ContainerDiv = styled.div`
+  display: flex;
+  gap: 20px;
+  flex-wrap: wrap;
+`;
+
+const TitleDiv = styled.div`
+  width: 400px;
+  height: 100px;
+  padding-left: 10px;
+
+  border: 1px solid lightgray;
+  border-radius: 10px;
+
+  display: flex;
+  align-items: center;
+`;
 
 export default App;
